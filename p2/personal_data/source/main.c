f8cf2187989673c89dff1f420c7a29803a583865
Mark Holland <mark@partiallogic.com>
2015-02-12 12:52:39 +0100

    End of lab session, stars moving differently, personal_data program working

---
+++ p2/personal_data/source/main.c
@@ -0,0 +1,55 @@
+/*---------------------------------------------------------------------------------
+
+	Basic template code for starting a DS app
+
+---------------------------------------------------------------------------------*/
+#include <nds.h>
+#include <stdio.h>
+#include <system.h>
+#include <inttypes.h>
+
+#define Color_Title "\x1b[31m" // Color Red
+#define Color_Value "\x1b[36m"  // To flush out prev settings
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+	consoleDemoInit();
+
+	char name[11] = {0};
+	int i;
+	for(i=0; i<PersonalData->nameLen; i++) {
+		name[i] = (char)(PersonalData->name[i] & 255); // get ascii-bits from utf-16 name
+		printf("%s", &name[i]);
+	}
+	printf("\n");
+
+	printf("%sAlarm hour:%s %u\n", Color_Title, Color_Value, PersonalData->alarmHour);
+	printf("%sAlarm minute:%s %u\n", Color_Title, Color_Value, PersonalData->alarmMinute);
+	printf("%sBirth Day:%s %u\n", Color_Title, Color_Value, PersonalData->birthDay);
+	printf("%sBirth Month:%s %u\n", Color_Title, Color_Value, PersonalData->birthMonth);
+
+	printf("%scalX1:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calX1);
+	printf("%scalX1px:%s %u\n", Color_Title, Color_Value, PersonalData->calX1px);
+	printf("%scalX2:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calX2);
+	printf("%scalX2px:%s %u\n", Color_Title, Color_Value, PersonalData->calX2px);
+	printf("%scalY1:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calY1);
+	printf("%scalY1px:%s %u\n", Color_Title, Color_Value, PersonalData->calY1px);
+	printf("%scalY2:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calY2);
+	printf("%scalY2px:%s %u\n", Color_Title, Color_Value, PersonalData->calY2px);
+
+	char message[27];
+	printf("%sUsers message:%s ", Color_Title, Color_Value);
+	for(i=0; i<PersonalData->messageLen; i++) {
+		message[i] = (char)(PersonalData->message[i] & 255);
+		printf("%s", &message[i]);
+	}
+	printf("\n");
+
+	printf("%sReal Time Clock offset:%s %" PRIu32 "\n", Color_Title, Color_Value, PersonalData->rtcOffset);
+	printf("%sTheme:%s %u\n", Color_Title, Color_Value, PersonalData->theme);
+
+	while(1) {
+		swiWaitForVBlank();
+	}
+
+}
---
+++ p2/star_field/source/main.c
@@ -0,0 +1,76 @@
+
+#include <nds.h> 
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NUM_STARS 40
+
+typedef struct
+{
+	int x;
+	int y;
+	int speed;
+	unsigned short color;
+}Star;
+
+
+Star stars[NUM_STARS];
+
+void MoveStar(Star* star) {
+	star->x += star->speed;
+	star->y += star->speed/rand() % 4 + 1;
+	//iprintf("pos:%d",star->x);
+	if(star->x >= SCREEN_WIDTH || star->x >= SCREEN_HEIGHT) {
+		star->color = RGB15(31,31,31); 
+		star->x = 0;
+		star->y = rand() % 192; 
+		star->speed = rand() % 4 + 1;
+	}
+}
+
+void ClearScreen(void) {
+	int i;
+
+	for(i = 0; i < 256 * 192; i++)
+		VRAM_A[i] = RGB15(0,0,0);
+	}
+	
+void InitStars(void) {
+	int i;
+	for(i = 0; i < NUM_STARS; i++) {
+		stars[i].color = RGB15(31,31,31); 
+		stars[i].x = rand() % 256; 
+		stars[i].y = rand() % 192;
+		stars[i].speed = rand() % 4 + 1;
+	}
+}
+	
+void DrawStar(Star* star) { 
+	VRAM_A[star->x + star->y * SCREEN_WIDTH] = star->color;
+}
+
+void EraseStar(Star* star) {
+	VRAM_A[star->x + star->y * SCREEN_WIDTH] = RGB15(0,0,0); 
+}
+
+int main(void) {
+	consoleDemoInit();
+	int i; 
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+	videoSetMode(MODE_FB0); 
+	vramSetBankA(VRAM_A_LCD);
+	ClearScreen();
+	InitStars();
+
+	//we like infinite loops in console dev!
+	while(1) {
+		swiWaitForVBlank();
+		for(i = 0; i < NUM_STARS; i++) {
+			EraseStar(&stars[i]);
+			MoveStar(&stars[i]);
+			DrawStar(&stars[i]); 
+		}
+	}
+	return 0; 
+}
