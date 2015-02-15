192b8db24f418435b71522567663c0d4b8f3291d
Mark Holland <mark@partiallogic.com>
2015-02-15 16:41:57 +0100

    Finished aligning output in personal data app

--- p2/personal_data/source/main.c
+++ p2/personal_data/source/main.c
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------
 
-	Basic template code for starting a DS app
+	Personal Data App
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
@@ -8,8 +8,8 @@
 #include <system.h>
 #include <inttypes.h>
 
-#define Color_Title "\x1b[31m" // Color Red
-#define Color_Value "\x1b[36m"  // To flush out prev settings
+#define Color_Title "\x1b[32m" // Color Green
+#define Color_Value "\x1b[38m"  // Color Grey
 //---------------------------------------------------------------------------------
 int main(void) {
 //---------------------------------------------------------------------------------
@@ -18,27 +18,31 @@ int main(void) {
 	char name[11] = {0};
 	int i;
 
-	printf("%sUsers name:%s ", Color_Title, Color_Value);
+	printf("%sUsers name:%9.9s ", Color_Title, Color_Value);
 	for(i=0; i<PersonalData->nameLen; i++) {
 		name[i] = (char)(PersonalData->name[i] & 255); // get ascii-bits from utf-16 name
 		printf("%s", &name[i]);
 	}
 	printf("\n");
 
-	printf("%sAlarm hour:%s %u\n", Color_Title, Color_Value, PersonalData->alarmHour);
-	printf("%sAlarm minute:%s %u\n", Color_Title, Color_Value, PersonalData->alarmMinute);
-	printf("%sBirth Day:%s %u\n", Color_Title, Color_Value, PersonalData->birthDay);
-	printf("%sBirth Month:%s %u\n", Color_Title, Color_Value, PersonalData->birthMonth);
+	printf("%sAlarm hour:%9s %u\n", Color_Title, Color_Value, PersonalData->alarmHour);
+	printf("%sAlarm minute:%7s %u\n", Color_Title, Color_Value, PersonalData->alarmMinute);
+	printf("%sBirth Day:%10s %u\n", Color_Title, Color_Value, PersonalData->birthDay);
+	printf("%sBirth Month:%8s %u\n", Color_Title, Color_Value, PersonalData->birthMonth);
 
-	printf("%scalX1:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calX1);
-	printf("%scalX1px:%s %u\n", Color_Title, Color_Value, PersonalData->calX1px);
-	printf("%scalX2:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calX2);
-	printf("%scalX2px:%s %u\n", Color_Title, Color_Value, PersonalData->calX2px);
-	printf("%scalY1:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calY1);
-	printf("%scalY1px:%s %u\n", Color_Title, Color_Value, PersonalData->calY1px);
-	printf("%scalY2:%s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calY2);
-	printf("%scalY2px:%s %u\n", Color_Title, Color_Value, PersonalData->calY2px);
+	printf("%scalX1:%14s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calX1);
+	printf("%scalX1px:%12s %u\n", Color_Title, Color_Value, PersonalData->calX1px);
+	printf("%scalX2:%14s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calX2);
+	printf("%scalX2px:%12s %u\n", Color_Title, Color_Value, PersonalData->calX2px);
+	printf("%scalY1:%14s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calY1);
+	printf("%scalY1px:%12s %u\n", Color_Title, Color_Value, PersonalData->calY1px);
+	printf("%scalY2:%14s %" PRIu16 "\n", Color_Title, Color_Value, PersonalData->calY2);
+	printf("%scalY2px:%12s %u\n", Color_Title, Color_Value, PersonalData->calY2px);
 
+	printf("%sRTC offset:%9s %" PRIu32 "\n", Color_Title, Color_Value, PersonalData->rtcOffset);
+	printf("%sTheme:%14s %u\n", Color_Title, Color_Value, PersonalData->theme);
+
+	//Print last because ugly defulat message on DeSmuMe
 	char message[27];
 	printf("%sUsers message:%s \n", Color_Title, Color_Value);
 	for(i=0; i<PersonalData->messageLen; i++) {
@@ -46,9 +50,6 @@ int main(void) {
 		printf("%s", &message[i]);
 	}
 	printf("\n");
-
-	printf("%sRTC offset:%s %" PRIu32 "\n", Color_Title, Color_Value, PersonalData->rtcOffset);
-	printf("%sTheme:%s %u\n", Color_Title, Color_Value, PersonalData->theme);
 
 	while(1) {
 		swiWaitForVBlank();
